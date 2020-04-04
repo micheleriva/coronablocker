0f0a24fc0329916619fa33054270d569b9eb5308
Michele Riva <master@micheleriva.it>
2020-04-04 14:42:22 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,120 @@
+const keywords = ["coronavirus", "covid-19", "covid"];
+
+const extractChannel = () => {
+  switch (window.location.hostname) {
+    case "www.facebook.com":
+      return "facebook";
+    case "www.linkedin.com":
+      return "linkedin";
+    default:
+      return null;
+  }
+};
+
+const computeFacebookQuery = (target) => {
+  switch (target) {
+    case "post":
+      return "div[role='article']";
+    case "link":
+      return "[role='link']";
+    default:
+      return "";
+  }
+};
+
+const computeLinkedinQuery = (target) => {
+  switch (target) {
+    case "feed":
+      return "feed-shared-update-v2";
+    case "news":
+      return "feed-shared-news-module__storyline";
+    default:
+      return "";
+  }
+};
+
+const removeFacebookItem = (item, target) => {
+  switch (target) {
+    case "post":
+      item.remove();
+      break;
+    case "link":
+      item.parentElement.parentElement.parentElement.remove();
+      break;
+    default:
+      break;
+  }
+};
+
+const removeLinkedinItem = (item, target) => {
+  switch (target) {
+    case "feed":
+    case "news":
+      item.remove();
+      break;
+    default:
+      break;
+  }
+};
+
+const removePosts = (channel, target) => {
+  let items;
+  let query;
+
+  switch (channel) {
+    case "facebook":
+      query = computeFacebookQuery(target);
+      items = document.querySelectorAll(query);
+      break;
+    case "linkedin":
+      query = computeLinkedinQuery(target);
+      items = document.getElementsByClassName(query);
+      break;
+    default:
+      break;
+  }
+
+  for (const item of items) {
+    const content = item.innerText.toLowerCase();
+
+    for (keyword of keywords) {
+      if (content.includes(keyword)) {
+        switch (channel) {
+          case "facebook":
+            removeFacebookItem(content, target);
+            break;
+          case "linkedin":
+            removeLinkedinItem(content, target);
+            break;
+          default:
+            break;
+        }
+        console.log(`[${+new Date()}] Removed an item about Covid-19`);
+      }
+    }
+  }
+};
+
+async function disableCorona() {
+  // Extract the current channel from the window location
+  const currentChannel = extractChannel();
+
+  // Execute the blocker only when on a specific channel
+  if (!currentChannel) return;
+
+  switch (currentChannel) {
+    case "facebook":
+      removePosts("facebook", "post");
+      removePosts("facebook", "link");
+      break;
+    case "linkedin":
+      removePosts("linkedin", "feed");
+      removePosts("linkedin", "news");
+      break;
+    default:
+      break;
+  }
+}
+
+document.addEventListener("load", disableCorona);
+document.addEventListener("DOMNodeInserted", disableCorona);
